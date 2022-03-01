fe29987ed9e18a3550c6cbb087b166707b930bc6
Mohuya748 <khandakermohuya@gmail.com>
2022-03-02 01:42:23 +0600

    detail added

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const displayPhone = data => {
                     <img src="${singleData.image}" class="card-img-top w-100" alt="...">
                       <h5 class="card-title">${singleData.phone_name}</h5>
                       <p class="card-text">${singleData.brand}</p>
-                
+                      <a class="btn btn-primary" onclick="loadPhoneDetail('${singleData.slug}')" href="#" role="button" >Details</a>
                     </div>
                    </div>
 
@@ -32,10 +32,27 @@ const displayPhone = data => {
 
     }
 
-    // }
-
 }
 
-const loadPhoneDetail = () => {
+const loadPhoneDetail = id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data))
+}
 
+const displayPhoneDetail = phone =>{
+    const phoneMoreDetails = document.getElementById("phone-details");
+    const div= document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    phoneMoreDetails.appendChild(div);
+    window.scrollTo(0,0);
 }
